cb0bcca3d902a67b5bab5b01cfab25beb8b7d084
Chloe Zhou <chloez0607@gmail.com>
2024-05-11 16:30:38 +0800

    added game trailer.

---
+++ src/hooks/useTrailers.ts
@@ -0,0 +1,16 @@
+import { useQuery } from "@tanstack/react-query";
+import ms from "ms";
+import { Trailer } from "../entities/Trailer";
+import ApiClient from "../services/client";
+
+const useTrailers = (gameId: number) => {
+  const apiClient = new ApiClient<Trailer>(`/games/${gameId}/movies`);
+
+  return useQuery({
+    queryKey: ["trailers", gameId],
+    queryFn: apiClient.getData,
+    staleTime: ms("24 hrs"),
+  });
+};
+
+export default useTrailers;
