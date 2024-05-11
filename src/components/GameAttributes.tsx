994ffd63194b904223719a598cbf5597f85f5b7c
Chloe Zhou <chloez0607@gmail.com>
2024-05-11 15:45:11 +0800

    implemented game attributes.

---
+++ src/components/GameAttributes.tsx
@@ -0,0 +1,37 @@
+import { SimpleGrid, Text } from "@chakra-ui/react";
+import { Game } from "../entities/Game";
+import CriticScore from "./CriticScore";
+import DefinitionItem from "./DefinitionItem";
+
+interface Props {
+  game: Game;
+}
+
+const GameAttributes = ({ game }: Props) => {
+  if (!game) return null;
+
+  return (
+    <SimpleGrid as={"dl"} columns={2}>
+      <DefinitionItem term="Platforms">
+        {game.parent_platforms.map(({ platform }) => (
+          <Text key={platform.id}>{platform.name}</Text>
+        ))}
+      </DefinitionItem>
+      <DefinitionItem term="Metascore">
+        <CriticScore score={game.metacritic} />
+      </DefinitionItem>
+      <DefinitionItem term="Genres">
+        {game.genres.map((genre) => (
+          <Text key={genre.id}>{genre.name}</Text>
+        ))}
+      </DefinitionItem>
+      <DefinitionItem term="Publishers">
+        {game.publishers.map((publisher) => (
+          <Text key={publisher.id}>{publisher.name}</Text>
+        ))}
+      </DefinitionItem>
+    </SimpleGrid>
+  );
+};
+
+export default GameAttributes;
--- src/entities/Game.ts
+++ src/entities/Game.ts
@@ -1,4 +1,6 @@
+import { Genre } from "./Genre";
 import { Platform } from "./Platform";
+import { Publisher } from "./Publisher";
 
 export interface Game {
   id: number;
@@ -11,4 +13,6 @@ export interface Game {
   rating_top: number;
   slug: string;
   description_raw: string;
+  publishers: Publisher[];
+  genres: Genre[];
 }
